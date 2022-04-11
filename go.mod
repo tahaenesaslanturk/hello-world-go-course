35aeec08cad8d91c8e86df0e4779bed2ecb51493
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-04-11 14:26:57 +0300

    first release

---
+++ go.mod
@@ -0,0 +1,13 @@
+module hello-world
+
+go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.7
+
+require (
+	github.com/alexedwards/scs v1.4.1 // indirect
+	github.com/alexedwards/scs/v2 v2.5.0 // indirect
+	github.com/justinas/nosurf v1.1.1 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+)
---
+++ pkg/render/render.go
@@ -0,0 +1,97 @@
+package render
+
+import (
+	"bytes"
+	"fmt"
+	"hello-world/pkg/config"
+	"hello-world/pkg/models"
+	"html/template"
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+var functions = template.FuncMap{}
+
+var app *config.AppConfig
+
+//	NewTemplates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+
+	return td
+}
+
+// RenderTemplate renders templates using html/template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	// if in development mode, make app.UseCache false to see changes immediately
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+	// get the template cache from the app config
+
+	//tc, err := CreateTemplateCache()
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("Could not get template from template")
+	}
+
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		fmt.Println("Error writing template to browser", err)
+	}
+
+	/* parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err = parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	} */
+}
+
+// CreateTemplateCache creates a template cache as map
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	myCache := map[string]*template.Template{}
+
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		fmt.Println("Page is currently", page)
+		ts, err := template.New(name).Funcs(functions).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+		}
+		myCache[name] = ts
+	}
+	return myCache, nil
+}
